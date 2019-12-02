aacf995d2249450ad4be66255e44e173670f19fc
Ubuntu <ubuntu@ip-172-31-82-202.ec2.internal>
2019-12-02 12:50:23 +0000

    linked category model to artifacts to display correct category

--- artifacthunters/settings.py
+++ artifacthunters/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
-#import env
+import env
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -41,6 +41,9 @@ INSTALLED_APPS = [
     'accounts',
     'artifacts',
     'cart',
+    'categories',
+    'histories',
+    'payment',
 ]
 
 MIDDLEWARE = [
@@ -129,8 +132,8 @@ STATICFILES_DIRS = (
 )
 
 # Media storage location
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 # Stripe key access
 STRIPE_PUBLISHABLE = os.environ.get("STRIPE_PUBLISHABLE")
