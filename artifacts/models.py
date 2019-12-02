e503520f28df3fc59b49ced32db72aeb4bdc7cf2
Ubuntu <ubuntu@ip-172-31-82-202.ec2.internal>
2019-12-02 12:45:06 +0000

    linked category model to artifacts to display correct category

--- artifacts/models.py
+++ artifacts/models.py
@@ -1,11 +1,11 @@
 from django.db import models
+from categories.models import Category
 
+# Model for artifacts
 class Artifact(models.Model):
-    """
-    This is the model for the artifacts to be sold/auctioned via the webapp
-    """
     
     name = models.CharField(max_length=100, default="")
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, null=False)
     description = models.TextField()
     reserve_price = models.DecimalField(max_digits=10, decimal_places=2)
     purchase_price = models.DecimalField(max_digits=10, decimal_places=2)
--- artifacts/views.py
+++ artifacts/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from artifacts.models import Artifact
+from categories.models import Category
 
 # Views for the artifacts in app
 def all_artifacts(request):
+    
     artifacts = Artifact.objects.all()
-    return render(request, "artifacts.html", {"artifacts": artifacts})
+    categories = Category.objects.all()
+    return render(request, "artifacts.html", {"artifacts": artifacts, "categories": categories})
