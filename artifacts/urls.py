b6bdb9be3b03a5ca5d4f9e9d30b3e26621169b72
Ubuntu <ubuntu@ip-172-31-82-202.ec2.internal>
2019-12-03 07:08:42 +0000

    update artifacts to resolve category error with travis

--- artifacts/urls.py
+++ artifacts/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url, include
-from artifacts.views import all_artifacts
+from artifacts.views import all_artifacts, view_artifact
 
+# URL patterns to display artifacts
 urlpatterns = [
-    url(r'^$', all_artifacts, name="artifacts")
+    url(r'^$', all_artifacts, name="artifacts"),
+    url(r'^view/(?P<id>\d+)', view_artifact, name="view_artifact"),
 ]
--- artifacts/views.py
+++ artifacts/views.py
@@ -8,3 +8,13 @@ def all_artifacts(request):
     artifacts = Artifact.objects.all()
     categories = Category.objects.all()
     return render(request, "artifacts.html", {"artifacts": artifacts, "categories": categories})
+
+def view_artifact(request, id):
+    artifact = Artifact.objects.filter(id)
+    print("------")
+    print(artifact)
+    
+    category = Category.objects.filter(artifact.category)
+    
+    print(category)
+    return render(request, "artifacts.html", {"artifacts": artifact, "categories": category})
