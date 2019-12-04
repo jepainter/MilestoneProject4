8e3a190c56107ef2bf5708a74e4adfdf6acba900
Ubuntu <ubuntu@ip-172-31-82-202.ec2.internal>
2019-12-04 07:49:13 +0000

    fixed lookup of histories for correct artifact

--- histories/models.py
+++ histories/models.py
@@ -10,8 +10,9 @@ class History(models.Model):
     artifact = models.ForeignKey(Artifact, on_delete=models.CASCADE, null=False)
     
     def __str__(self):
-        return "{0} - {1}".format(
+        return "{0} - {1}: {2}".format(
             self.id,
+            self.artifact.id,
             self.artifact.name
         )
 
@@ -21,6 +22,7 @@ class HistoryEvent(models.Model):
     Model for specific event within a history
     """
     history_id = models.ForeignKey(History, on_delete=models.CASCADE, null=False)
+    #artifact = models.ForeignKey(Artifact, on_delete=models.CASCADE, null=False)
     event_description = models.TextField(blank=False)
     event_year = models.IntegerField(blank=False)
     event_era = models.CharField(max_length=50, blank=False)
@@ -28,6 +30,7 @@ class HistoryEvent(models.Model):
     def __str__(self):
         return "{0} - {1} {2}: {3}".format(
             self.history_id,
+     #       self.artifact.id,
             self.event_year,
             self.event_era,
             self.event_description,
--- histories/views.py
+++ histories/views.py
@@ -8,13 +8,18 @@ def view_history(request, id):
     artifact = History.objects.filter(artifact_id=id)
     print("")
     for detail in artifact:
-        print(detail.artifact.name)
-        print(detail.artifact.id)
+        print("## Artifact query ##")
+        print("History Id: " + str(detail.id))
+        print("Artifact Name: " + str(detail.artifact.name))
+        print("Artifact Id: " + str(detail.artifact.id))
     
     print("Artifact: " + str(artifact))
-    events = HistoryEvent.objects.filter(history_id=id)
+    events = HistoryEvent.objects.filter(history_id=detail.id)
     for event in events:
         print("")
+        print("## Event query ##")
+    #    print("Artifact ID :" + str(event.artifact.id))
+    #    print("Artifact Name:" + str(event.artifact.name))
         print("Event Year: " + str(event.event_year))
         print("Event Era: " + str(event.event_era))
         print("Event Description: " + str(event.event_description))
