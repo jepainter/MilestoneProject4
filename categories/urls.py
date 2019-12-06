159d7805682734f5b69369a243cb32451faba817
Ubuntu <ubuntu@ip-172-31-82-202.ec2.internal>
2019-12-06 08:21:22 +0000

    added view to categories that only render specific artifacts withina category

--- categories/urls.py
+++ categories/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url, include
-from categories.views import all_categories
+from categories.views import all_categories, view_category
 
 # URL patterns to display categories
 urlpatterns = [
-    url(r'^$', all_categories, name="categories")
+    url(r'^$', all_categories, name="categories"),
+    url(r'^view/(?P<id>\d+)', view_category, name="view_category")
 ]
--- categories/views.py
+++ categories/views.py
@@ -1,8 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from categories.models import Category
+from artifacts.models import Artifact
 
 # Views for the categories in app
 def all_categories(request):
     
     categories = Category.objects.all()
     return render(request, "categories.html", {"categories": categories})
+
+def view_category(request, id):
+    """
+    View to get one category and associated artifacts and render html
+    """
+    
+    category = Category.objects.filter(id=id)
+    print("")
+    print("## Inside single category function ##")
+    print("Category: " + str(category))
+    
+    for category_detail in category:
+        
+        print("Category ID: " + str(category_detail.id))
+        print("Category Name: " + str(category_detail.category_name))
+        print("Category Description: " + str(category_detail.category_description))
+    
+    artifacts = Artifact.objects.filter(category=category_detail.id)
+    
+    print("Artifacts: " + str(artifacts))
+    
+    return render(request, "artifacts.html", {"artifacts": artifacts, "categories": category})
