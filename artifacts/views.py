282f69b448ebc2a5ea1bb701ad4ef0aceec8d8c4
Ubuntu <ubuntu@ip-172-31-82-202.ec2.internal>
2019-12-12 14:13:46 +0000

    commit migrations files to github to resolve travis error

--- artifacts/views.py
+++ artifacts/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render
 from artifacts.models import Artifact
-from categories.models import Category
+#from categories.models import Category
 
 # Views for the artifacts in app
 def all_artifacts(request):
     
     artifacts = Artifact.objects.all()
-    categories = Category.objects.all()
-    return render(request, "artifacts.html", {"artifacts": artifacts, "categories": categories})
+#    categories = Category.objects.all()
+    return render(request, "artifacts.html", {"artifacts": artifacts})
 
 def view_artifact(request, id):
     artifact = Artifact.objects.filter(id=id)
@@ -17,11 +17,22 @@ def view_artifact(request, id):
     for artifact_detail in artifact:
         print("Artifact Name: " + str(artifact_detail.name))
         print("Artifact Id: " + str(artifact_detail.id))
-        print("Category Id: " + str(artifact_detail.category))
+        print("Category Object: " + str(artifact_detail.category))
+        print("Category Id: " + str(artifact_detail.category.id))
+        print("Category Description: " + str(artifact_detail.category.category_description))
     
-    category = Category.objects.filter(category_name=artifact_detail.category)
+    #category = Category.objects.filter(category_name=artifact_detail.category)
     #categories = Category.objects.all()
     
-    print("Category: " + str(category))
+    print("")
+    print("Testing access from artifact to category via foreignkey")
+    print("Artifact: " + str(artifact))
     
-    return render(request, "artifact_detail.html", {"artifact": artifact, "category": category})
+    
+    print("Exit test")
+    print("")
+    
+    
+    #print("Category: " + str(category))
+    
+    return render(request, "artifact_detail.html", {"artifact": artifact})
