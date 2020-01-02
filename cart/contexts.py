17d2289e23bd425d7d4ec321e985a5d1154183af
Ubuntu <ubuntu@ip-172-31-82-202.ec2.internal>
2020-01-02 13:16:30 +0000

    changed cart app to include secondary module for adding bids for payment

--- cart/contexts.py
+++ cart/contexts.py
@@ -8,35 +8,57 @@ def cart_contents(request):
     """
     
     cart = request.session.get("cart", {})
+    bid_cart = request.session.get("bid_cart", {})
     cart_items = []
+    bid_cart_items = []
     total = 0
     artifact_count = 0
     
     for id, quantity in cart.items():
+  #      print(id)
+  #      print(cart[id])
+  #      print(cart[id]['price'])
+  #      print(cart[id]['quantity'])
+  #      print(cart[id]['bid'])
         artifact = get_object_or_404(Artifact, pk=id)
-        try:
-            bid = BidLineItem.objects.filter(bid_event__artifact=id).get(bid_highest=True)
-            total += quantity * bid.bid_amount
-            artifact_count += quantity
-            cart_items.append({
-                "id": id,
-                "quantity": quantity,
-                "artifact": artifact,
-                "bid": bid,
-            })
-        
-        except:
-            total += quantity * artifact.purchase_price
-            artifact_count += quantity
-            cart_items.append({
-                "id": id,
-                "quantity": quantity,
-                "artifact": artifact,
-                "bid": "",
-            })
+        total += quantity * artifact.purchase_price
+        artifact_count += quantity
+        cart_items.append({
+            "id": id,
+            "quantity": quantity,
+            "artifact": artifact,
+#            "price": cart[id]['price'], 
+        })
+            
+    print("Cart Items: " + str(cart_items))
+    
+#    cart_second = request.session.get("cart_second", {})
+#    cart_items_two = []
+  #  print("Cart second: " + str(cart_second))
+    
+    for id, bid_id in bid_cart.items():
+  #      print(id)
+  #      print(cart_second[id])
+  #      print(cart_second[id]['price'])
+  #      print(cart_second[id]['quantity'])
+        bid = BidLineItem.objects.get(pk=bid_id)    
+        #artifact = get_object_or_404(Artifact, pk=id)
+        total += bid.bid_quantity * bid.bid_amount
+        artifact_count += bid.bid_quantity
+        bid_cart_items.append({
+            "id": id,
+            "bid_id": bid_id,
+            "quantity": bid.bid_quantity,
+            "artifact": bid.bid_event.artifact,
+            "bid_amount" : bid.bid_amount,
+        })
+    
+    print("Bid Cart Items: " + str(bid_cart_items))
+    
         
     return {
         "cart_items": cart_items,
+        "bid_cart_items": bid_cart_items,
         "total": total,
         "artifact_count": artifact_count
     }
--- cart/views.py
+++ cart/views.py
@@ -1,23 +1,55 @@
 from django.shortcuts import render, redirect, reverse
+from artifacts.models import Artifact
+from bids.models import BidLineItem
 
 # Views to render the cart contents and adjust
 def view_cart(request):
     """
     Contents of cart is rendered to html
     """
+#   cart = {}
+#    request.session["cart"] = cart
+#    bid_cart = {}
+#    request.session["bid_cart"] = bid_cart
+    
     return render(request, "cart.html")
 
 def add_to_cart(request, id):
     """
     Add quantity to the specific product in the cart
     """
+    
+    print("")
+    print("## Inside add purchase to cart function ##")
+    print("Artifact Id: " + str(id))
+    
+    #cart_second = {}
+    
+    #request.session["cart_second"] = cart_second
+    
     quantity = int(request.POST.get("quantity"))
+    #artifact = Artifact.objects.get(pk=id)
+    #price = float(artifact.purchase_price)
+    
+    #print("artifact: " + str(artifact))
+    #print("price: " + str(price))
+    
     cart = request.session.get("cart", {})
+    
     cart[id] = cart.get(id, quantity)
+#    cart[id] = {
+        #"price": float(artifact.purchase_price),
+ #       "quantity": int(request.POST.get("quantity")),
+        #"bid": ""
+ #   }
+    print("Cart Contents: " + str(cart))
     
     request.session["cart"] = cart
     
-    return redirect(reverse("index"))
+    print("## Exiting add purchase to cart function ##")
+    print("")
+    
+    return redirect(reverse("view_cart"))
     
 def adjust_cart(request, id):
     """
@@ -41,8 +73,53 @@ def remove_item_from_cart(request, id):
     """
     
     cart = request.session.get("cart", {})
+    #bid_cart = request.session.get("bid_cart", {})
+    #bid_cart.pop(id)
     cart.pop(id)
     
     request.session["cart"] = cart
+    #request.session["bid_cart"] = bid_cart
+    
+    return redirect(reverse("view_cart"))
+
+def add_bid_to_cart(request, id):
+    """
+    Process successful bid to add to cart
+    """
+    print("")
+    print("## Inside add bid to cart function ##")
+    print("Bid Line Id: " + str(id))
+    
+    bid_line_item = BidLineItem.objects.get(pk=id)
+    print("Bid Line Item: "+ str(bid_line_item))
+    
+  #  artifact = bid_line_item.bid_event.artifact.id    
+  #  print("Artifact: "+ str(artifact))
+    
+    bid_cart = request.session.get("bid_cart", {})
+    
+   # bid_cart[bid_line_item.bid_event.artifact.id] = {
+#        "bid": bid_line_item.id
+#    }
+    
+    bid_cart[bid_line_item.bid_event.artifact.id] = bid_cart.get(bid_line_item.bid_event.artifact.id, id)
+    print("Bid_Cart Contents: " + str(bid_cart))
+    
+    request.session["bid_cart"] = bid_cart
+    
+    print("## Exiting add bid to cart function ##")
+    print("")
+    
+    return redirect(reverse("view_cart"))
+    
+def remove_bid_from_cart(request, id):
+    """
+    Remove an bid from the cart for later payment
+    """
+    
+    bid_cart = request.session.get("bid_cart", {})
+    bid_cart.pop(id)
+    
+    request.session["bid_cart"] = bid_cart
     
     return redirect(reverse("view_cart"))
