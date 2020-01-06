95bf7d0cbf99129378f3bc7d70c3d2383a0724b3
Ubuntu <ubuntu@ip-172-31-82-202.ec2.internal>
2020-01-06 15:00:46 +0000

    upload of artifacts app before refactoring and clean up of code

--- artifacts/models.py
+++ artifacts/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from categories.models import Category
 
-# Model for artifacts
+# Model for artifact to be listed in database
 class Artifact(models.Model):
     
     name = models.CharField(max_length=100, default="")
@@ -10,6 +10,8 @@ class Artifact(models.Model):
     reserve_price = models.DecimalField(max_digits=10, decimal_places=2)
     purchase_price = models.DecimalField(max_digits=10, decimal_places=2)
     quantity = models.IntegerField()
+    #sold_out = models.BooleanField(default=False, null=False)
+    #bidding_allowed = models.BooleanField(default=False, null=False)
     image = models.ImageField(upload_to="images")
     
     def __str__(self):
