db459e108d987472f081c473d1b78403f239da53
Ubuntu <ubuntu@ip-172-31-82-202.ec2.internal>
2020-01-20 11:54:55 +0000

    updated layout and styling of accounts html pages

--- accounts/forms.py
+++ accounts/forms.py
@@ -7,24 +7,28 @@ class UserLoginForm(forms.Form):
     """
     Form for user to input info to log in
     """
-    username = forms.CharField()
-    password = forms.CharField(widget=forms.PasswordInput)
+    
+    username = forms.CharField(label="Username or Email:")
+    password = forms.CharField(label="Password:", widget=forms.PasswordInput)
 
 
 class UserRegistrationForm(UserCreationForm):
     """
     Form for new user to input info to register
     """
+    
+    username = forms.CharField(label="Username:")
+    email = forms.CharField(label="Email:", widget=forms.EmailInput)
     password1 = forms.CharField(
-        label="Password",
+        label="Password:",
         widget=forms.PasswordInput)
     password2 = forms.CharField(
-        label="Confirm Password",
+        label="Confirm Password:",
         widget=forms.PasswordInput)
     
     class Meta:
         model = User
-        fields = ["email", "username", "password1", "password2"]
+        fields = ["username", "email", "password1", "password2"]
     
     def clean_email(self):
         email = self.cleaned_data.get("email")
@@ -46,3 +50,27 @@ class UserRegistrationForm(UserCreationForm):
             raise forms.ValidationError("Your passwords do not match.")
         
         return password2
+
+
+class ProfileUpdateForm(forms.Form):
+    """
+    Form for update of user details
+    """
+    
+    username = forms.CharField(
+        label="Username:",
+        required=True,
+        widget=forms.TextInput(
+            attrs={
+                "placeholder": "What username do you want to use..."
+            })
+        )
+    email = forms.CharField(
+        label="Email:",
+        required=True,
+        widget=forms.EmailInput(
+            attrs={
+                "placeholder": "What email do you want to use..."
+            })
+        )
+    
