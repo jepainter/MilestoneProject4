b119d513599b6c8746e8adde06ae5705e2d1297e
Ubuntu <ubuntu@ip-172-31-82-202.ec2.internal>
2020-01-22 09:12:49 +0000

    fix bids migrations error received from Travis

--- bids/migrations/0001_initial.py
+++ bids/migrations/0001_initial.py
@@ -9,6 +9,7 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
 
     initial = True
+    atomic = False
 
     dependencies = [
         ('artifacts', '0004_auto_20191203_0730'),
