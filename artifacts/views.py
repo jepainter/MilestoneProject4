473b5e1bb76d4ca9f1fd98eedf85b69d2202f8c2
Ubuntu <ubuntu@ip-172-31-82-202.ec2.internal>
2020-01-23 08:26:09 +0000

    comments to views and minor change to artifact_details page

--- artifacts/views.py
+++ artifacts/views.py
@@ -12,7 +12,6 @@ def all_artifacts(request):
     """
     Function to call all instances of artifacts and render to page.
     """
-    
     artifacts = Artifact.objects.all()
     
     return render(request, "artifacts.html", {"artifacts": artifacts})
@@ -21,16 +20,11 @@ def all_artifacts(request):
 def view_artifact(request, id):
     """
     Function to view detail relating to artifact, such as history,
-    bid information. Nested try except loops to reduce calls to database
-    if possible. Checks current cart for an existing artifact in
-    order to render relevant options in html (bid or purchase).
+    bid information and reviews. Nested try except loops to reduce 
+    calls to database if possible. Checks current cart for an existing 
+    artifact in order to render relevant options in html (bid or purchase).
     """
-
-    history_events = HistoryEvent.objects.filter(history_id__artifact=id)
-    ownership = OrderLineItem.objects.filter(artifact=id).filter(owner_id=request.user.id).first()
-    reviews = ReviewLineItem.objects.filter(review_id__artifact=id)
-    reviewed = False
-    
+    # Retrieval of bid information and artifact for display
     try:
         bid_line_item = BidLineItem.objects.filter(
             bid_user=request.user.id).get(bid_event__artifact=id)
@@ -38,7 +32,7 @@ def view_artifact(request, id):
         artifact = bid_line_item.bid_event.artifact
     except:
         bid_line_item = ""
-        
+        # Retrieval of artifact detail if above try fails
         try:
             bid_event = BidEvent.objects.get(artifact=id)
             artifact = bid_event.artifact
@@ -46,16 +40,23 @@ def view_artifact(request, id):
             bid_event = ""
             artifact = get_object_or_404(Artifact, pk=id)
     
+    # Check if item in cart to determine bidding and purchasing options
     cart = request.session.get("cart", {})
     artifact_in_cart = {}
-    
     for artifact_id, artifact_quantity in cart.items():
         if artifact_id == id:
             artifact_in_cart = {
                 "artifact_id" : artifact_id,
                 "artifact_quantity": artifact_quantity,
                 }
-                
+    
+    # Check to determine if user is owner and determine if user have reviewed           
+    history_events = HistoryEvent.objects.filter(
+        history_id__artifact=id).order_by("event_era").order_by("event_year")
+    ownership = OrderLineItem.objects.filter(
+        artifact=id).filter(owner_id=request.user.id).first()
+    reviews = ReviewLineItem.objects.filter(review_id__artifact=id)
+    reviewed = False
     if ownership != None:
         owner = True
         for review in reviews:
